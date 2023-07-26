2fe961f7c501a7c8e8c0efc97fc07e93a6ef3369
sajitheranda <118637181+sajitheranda@users.noreply.github.com>
2023-07-26 23:14:49 +0530

    Update file

--- frontend-main/src/data.js
+++ frontend-main/src/data.js
@@ -2,7 +2,7 @@ const defaultGasValues = { onlyGas: 10, emptyTank: 0 };
 
 const gasTypeValues = {
   buddy: { onlyGas: 10, emptyTank: 1 },
-  budget: { onlyGas: 10, emptyTank: 4.5 },
+  budget: { onlyGas: 5, emptyTank: 3 },
   regular: { onlyGas: 12.5, emptyTank: 13.5 },
 };
 
